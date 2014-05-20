ed838e36d35a99ba53ec557cd5e7f96539b0134b
epowell <epowell@zenoss.com>
2014-05-20 12:49:50 -0500

    Version 2.0.2dev -> 2.1.0dev to allow hotfixes

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 # or saved.  Do not modify them directly here.
 # NB: PACKAGES is deprecated
 NAME = "ZenPacks.zenoss.CalculatedPerformance"
-VERSION = "2.0.2dev"
+VERSION = "2.1.0dev"
 AUTHOR = "Zenoss"
 LICENSE = "GPLv2"
 NAMESPACE_PACKAGES = ['ZenPacks', 'ZenPacks.zenoss']
